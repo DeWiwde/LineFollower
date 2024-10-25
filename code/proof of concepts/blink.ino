b5dab08295a786d2ddc0e6587ef8dd1fc4ebfe0f
Yentl <yentl.dewilde07@gmail.com>
2024-10-25 16:31:05 +0200

    blink.ino
    Een klein stukje code waarin we een lamp doen blinken. Hiermee kan nagegaan worden of het compilen en uploaden naar de microcontroller mogelijk is.

--- code/proof of concepts/blink.ino
+++ code/proof of concepts/blink.ino
@@ -4,16 +4,16 @@ int blink = 13;
 
 void setup() 
 {
-    pinMode (blink, OUTPUT);
+    pinMode (blink, OUTPUT); //Pin 13 wordt in deze code aangeduid als de pin waarop de led aangesloten is.
 }
 
 void loop() 
 {
  digitalWrite (blink, HIGH);
- Serial.println("Lamp gaat aan!");
- delay(2000);
+ Serial.println("Lamp gaat aan!"); //In de seriële monitor kan nagegaan worden of de loop werkt (en de lamp al dan niet zou AAAN moeten gaan).
+ delay(2000); //Delay van 2seconden!
 
  digitalWrite (blink, LOW);
- Serial.println ("Lamp gaat uit!");
- delay(2000);
+ Serial.println ("Lamp gaat uit!"); //In de seriële monitor kan nagegaan worden of de loop werkt (en de lamp al dan niet zou UIT moeten gaan).
+ delay(2000);//Idem ditto een delay van 2seconden!
 }
