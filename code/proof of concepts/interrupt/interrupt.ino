3c81d424f6e2eee73d8dd7d00e5a07d54d39318b
Yentl <yentl.dewilde07@gmail.com>
2025-08-31 22:09:33 +0200

    Add files via upload

---
+++ code/proof of concepts/interrupt/interrupt.ino
@@ -0,0 +1,31 @@
+const int buttonPin = 2;
+const int ledPin = 4;
+volatile int buttonState = LOW;
+
+void setup() 
+{
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonInterrupt, RISING);
+}
+
+void loop() 
+{
+  // De hoofdcode hier kan andere taken uitvoeren terwijl het wacht op de interrupt
+}
+
+void buttonInterrupt() //eenmaal op knop gedrukt, blijft deze brandden, gaat niet meer uit tot hij gereset is.
+{
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) 
+  {
+    digitalWrite(ledPin, HIGH);
+    Serial.println("High");
+  } 
+  else 
+  {
+    digitalWrite(ledPin, LOW);
+    Serial.println("Low");
+  }
+}
